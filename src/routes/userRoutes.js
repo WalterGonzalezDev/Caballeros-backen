217c49ff4c25cb421c317c3e16ad690508ffbd47
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-05 16:08:38 -0500

    Se agrega validaciones

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import UserController from '../application/controllers/UserController.js';
+import { validateRegister, validateLogin, handleValidationErrors } from '../application/middlewares/validateUser.js';
 
 const router = express.Router();
 const userController = new UserController();
 
-router.post('/register', userController.register.bind(userController));
+router.post('/register', handleValidationErrors, validateRegister, userController.register);
+router.post('/login', handleValidationErrors, validateLogin, userController.login);
 
 export default router;
