7583306f964d4a3c913f630c7ddf8e0780012500
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-05 16:04:12 -0500

    Se crear middlewares de validación

---
+++ src/middlewares/validators.js
@@ -0,0 +1,21 @@
+import { body, validationResult } from "express-validator";
+
+export const validateSignup = [
+    body("name").notEmpty().withMessage("El nombre es obligatorio"),
+    body("email").isEmail().withMessage("El email es obligatorio"),
+    body("password").isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 8 caracteres"),
+    body("role").notEmpty().withMessage("El rol es obligatorio"),
+];
+export const validateLogin = [
+    body("email").isEmail().withMessage("El email debe ser válido"),
+    body("password").isLength({ min: 6 }).withMessage("La contraseña es obligatoria"),
+];
+
+export const handleValidationErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+  
