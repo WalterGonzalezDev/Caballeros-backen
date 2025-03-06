7bc728e97a67438a8c15884da8ff3921e9b23212
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-06 10:12:17 -0500

    Se actualiza

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -1,21 +1,21 @@
-import { body, validationResult } from "express-validator";
+import { body, validationResult } from 'express-validator';
 
 export const validateRegister = [
-    body("name").notEmpty().withMessage("El nombre es obligatorio"),
-    body("email").isEmail().withMessage("El email es obligatorio"),
-    body("password").isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 8 caracteres"),
-    body("role").notEmpty().withMessage("El rol es obligatorio"),
+  body('name').notEmpty().withMessage('El nombre es obligatorio.'),
+  body('email').isEmail().withMessage('El correo debe ser válido.'),
+  body('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres.'),
+  body('roleId').notEmpty().withMessage('El rol es obligatorio.'),
 ];
+
 export const validateLogin = [
-    body("email").isEmail().withMessage("El email debe ser válido"),
-    body("password").isLength({ min: 6 }).withMessage("La contraseña es obligatoria"),
+  body('email').isEmail().withMessage('El correo debe ser válido.'),
+  body('password').notEmpty().withMessage('La contraseña es obligatoria.'),
 ];
 
 export const handleValidationErrors = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
 };
-  
