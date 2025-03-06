455928b6fe2b702783a1f6266e146ea56fa9c1cd
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-06 10:11:44 -0500

    Se cambia handleValidationErrors porvalidateRegister y validateLogin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,11 +2,10 @@ import express from 'express';
 import UserController from '../application/controllers/UserController.js';
 import { validateRegister, validateLogin, handleValidationErrors } from '../middlewares/validators.js';
 
-
 const router = express.Router();
 const userController = new UserController();
 
-router.post('/register', handleValidationErrors, validateRegister, userController.register);
-router.post('/login', handleValidationErrors, validateLogin, userController.login);
+router.post('/register', validateRegister, handleValidationErrors, userController.register.bind(userController));
+router.post('/login', validateLogin, handleValidationErrors, userController.login.bind(userController));
 
 export default router;
