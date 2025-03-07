9729d633fa49680a3c033efabac7a104b4b2d837
Walter González <waltergonzalezdeveloper@gmail.com>
2025-03-07 10:28:32 -0500

    Se cabia bcrypt por bcryptjs

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import logger from '../config/logger.js';
 
 dotenv.config();
 
@@ -12,7 +13,12 @@ export const hashPassword = async (password) => {
 };
 
 export const comparePassword = async (password, hashedPassword) => {
-  return await bcrypt.compare(password, hashedPassword);
+  logger.info(`password: ${password}, hashedPassword: ${hashedPassword}`);
+  if (hashedPassword.startsWith('$2y$') || hashedPassword.startsWith('$2a$') || hashedPassword.startsWith('$2b$')) {
+    return await bcrypt.compare(password, hashedPassword);
+  } else {
+    return password === hashedPassword;
+  }
 };
 
 export const generateToken = (user) => {
